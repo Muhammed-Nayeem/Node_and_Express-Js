b8dc7ce0da17bc62659b806e5c4e74194831b52e
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-01-10 12:49:43 +0600

    Create Fake Json Data for Phones Server

--- phones-server/index.js
+++ phones-server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const phones = require("./phones.json");
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Express Js Phones Server Creating....!");
+});
+
+//phones data api:
+app.get("/phones", (req, res) => {
+  res.send(phones);
 });
 
 app.listen(port, () => {
