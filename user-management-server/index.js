ca7126f9e37f2ed0f7aebbd0a530b545d40c40a3
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-01-11 21:59:54 +0600

    Users data create

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -3,8 +3,18 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+  { _id: 1, name: "Shabana", email: "shabana@gmail.com" },
+  { _id: 2, name: "Shabnoor", email: "shabnoor@gmail.com" },
+  { _id: 3, name: "Sabila", email: "sabila@gmail.com" },
+];
+
 app.get("/", (req, res) => {
   res.send("User Management Server Created!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.listen(port, () => {
