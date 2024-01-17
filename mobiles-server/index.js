464d0630c54c7547b9e137543c4ad43f32dc1eb2
Muhammed Nayeem <mohammednayeem808@gmail.com>
2024-01-17 23:52:58 +0600

    All phones data get api

--- mobiles-server/index.js
+++ mobiles-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mobiles = require("./phones.json");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,11 @@ app.use(express.json());
 //route url get api:
 app.get("/", (req, res) => {
   res.send("Mobiles Server is creating...!");
+});
+
+//mobiles url to find all Phones get api:
+app.get("/mobiles", (req, res) => {
+  res.send(mobiles);
 });
 
 //port listener api:
